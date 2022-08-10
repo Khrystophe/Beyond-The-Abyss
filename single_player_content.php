1c110aceb78b5dad1029b55673ded2c1335bd0e8
chris <gitchris2@gmail.com>
2022-08-10 21:18:51 +0200

    deck

--- single_player_content.php
+++ single_player_content.php
@@ -115,59 +115,27 @@ $contents = $req->fetch();
           </div>
 
           <div class="column2">
-            <p>Bilbo Baggins is swept into a quest to reclaim the lost Dwarf Kingdom of Erebor from the fearsome dragon Smaug. Approached out of the blue by the wizard Gandalf the Grey, Bilbo finds himself joining a company of thirteen dwarves led by the legendary warrior, Thorin Oakenshield. Their journey will take them into the Wild; through... <a href="#">read more</a></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quibusdam qui, molestiae molestias fuga, beatae enim illo fugit laboriosam fugiat eius magnam ex deserunt esse in corporis sed ipsum error! Minima facere voluptatum nesciunt harum, maiores repudiandae! Repellat nisi esse aperiam dignissimos optio et pariatur. Aperiam voluptatum tempore soluta iusto.<a href="#">read more</a></p>
           </div>
         </div>
 
         <div class='deck'>
           <div class='single_player_card'>
             <div class='cardHeader'>
-              <i class='fab fa-instagram cardHeader_type'></i>
               <span class='cardHeader_account'>@hongkongfp</span>
-              <span class='cardHeader_date'>Jul 1 6:03</span>
             </div>
             <div class='cardBody'>
               <p class='cardText'>On Tuesday, China’s National People’s Congress Standing Committee unanimously passed a controversial national security law for Hong Kong. The law, which criminalises acts of secession, subversion, foreign interference and terrorism, was promulgated and gazetted on the same day with immediate effect. Details were only revealed late at night as it was directly inserted into the Annex III of the semi-autonomous region’s mini-constitution, bypassing the local legislature.
-                <span class='cardText_highlight'>#hongkong</span> <span class='cardText_highlight'>#china</span> <span class='cardText_highlight'>#humanrights</span> <span class='cardText_highlight'>#hk</span>
               </p>
-              <section class='cardStats'>
-                <span class='cardStats_stat cardStats_stat-likes'>2155 <i class='far fa-heart fa-fw'></i></span>
-                <span class='cardStats_stat cardStats_stat-comments'>87 <i class='far fa-comment fa-fw'></i></span>
-                </span>
             </div>
           </div>
 
           <div class='single_player_card'>
             <div class='cardHeader'>
-              <i class='fab fa-facebook-f cardHeader_type'></i>
               <span class='cardHeader_account'>@hongkongfp</span>
               <span class='cardHeader_date'>Jul 2 11:49</span>
-            </div>
-            <div class='cardBody'>
-              <p class='cardText'>“Liberate Hong Kong; revolution of our times,” is pro-independence, secessionist or subversive, and thus criminalised under the newly-enacted national security law, the government says.
-                Law scholar Benny Tai said: "The reality is, under the Hong Kong national security law, every sentence said by every person at every moment could touch the red line and be said as violating the law. How could the national security law only targets a small group of people.”</p>
-              <section class='cardStats'>
-                <span class='cardStats_stat cardStats_stat-likes'>857 <i class='far fa-heart fa-fw'></i></span>
-                <span class='cardStats_stat cardStats_stat-comments'>54 <i class='far fa-comment fa-fw'></i></span>
-                <span class='cardStats_stat cardStats_stat-shares'>410 <i class='far fa-share fa-fw'></i></span>
-                </span>
-            </div>
-          </div>
-
-          <div class='single_player_card'>
-            <div class='cardHeader'>
-              <i class='fab fa-twitter cardHeader_type'></i>
-              <span class='cardHeader_account'>@hongkongfp</span>
-              <span class='cardHeader_date'>Jul 6 11:26</span>
-            </div>
-            <div class='cardBody'>
-              <p class='cardText'>Hong Kong gov’t tells schools to remove books breaching security law
-                <span class='cardText_highlight'>https://t.co/JbyrMwxexU</span>
-              </p>
-              <section class='cardStats'>
-                <span class='cardStats_stat cardStats_stat-likes'>96 <i class='far fa-heart fa-fw'></i></span>
-                <span class='cardStats_stat cardStats_stat-retweets'>110 <i class='far fa-retweet fa-fw'></i></span>
-                </span>
+              <span class='cardStats_stat cardStats_stat-likes'>857 <i class='far fa-heart fa-fw'></i></span>
+              <span class='cardStats_stat cardStats_stat-comments'>54 <i class='far fa-comment fa-fw'></i></span>
             </div>
           </div>
         </div>
