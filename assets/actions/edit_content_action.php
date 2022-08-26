7346cd79cddcca8859368b89456654cd7ccfbdd2
chris <gitchris2@gmail.com>
2022-08-27 01:34:04 +0200

    edit reduction

--- assets/actions/edit_content_action.php
+++ assets/actions/edit_content_action.php
@@ -2,26 +2,6 @@
 session_start();
 require('../require/co_bdd.php');
 
-if (isset($_FILES) && !empty($_FILES)) {
-    if (array_key_exists('content', $_FILES)) {
-        if ($_FILES['content']['error'] == 0) {
-            if (in_array($_FILES['content']['type'], ['video/mp4', 'image/png', 'image/jpeg'])) {
-                if ($_FILES['content']['size'] <= 128000000) {
-                    $content = uniqid() . '.' . pathinfo($_FILES['content']['name'], PATHINFO_EXTENSION);
-                    move_uploaded_file($_FILES['content']['tmp_name'], '../contents_img/' . $content);
-                } else {
-                    var_dump('Le fichier est trop volumineux…');
-                    exit;
-                }
-            } else {
-                echo 'Le type mime du fichier est incorrect…';
-            }
-        } else {
-            echo 'Le fichier n\'a pas pu être récupéré…';
-        }
-    }
-}
-
 $req = $bdd->prepare('SELECT credits FROM users WHERE id = :id');
 $req->execute(array(
     ':id' => $_POST['id_users']
@@ -43,14 +23,32 @@ $free_content = $_POST['free_content'];
 if (!isset($free_content)) {
     if ($_POST['category'] == 'Tutorial') {
         $newPrice = 15;
+        $author_credits += 30;
     } else if ($_POST['category'] == 'Performance') {
         $newPrice = 5;
+        $author_credits += 10;
     } else if ($_POST['category'] == 'Sheet Music') {
         $newPrice = 10;
+        $author_credits += 20;
     }
 } else {
-    $newPrice = 0;
+    if ($_POST['category'] == 'Tutorial') {
+        $newPrice = 0;
+        $author_credits += 30;
+    } else if ($_POST['category'] == 'Performance') {
+        $newPrice = 0;
+        $author_credits += 10;
+    } else if ($_POST['category'] == 'Sheet Music') {
+        $newPrice = 0;
+        $author_credits += 20;
+    }
 }
+
+$req = $bdd->prepare('UPDATE users SET credits = :credits WHERE users.id = :users_id');
+$req->execute(array(
+    ':credits' => $author_credits,
+    ':users_id' => $_POST['id_users']
+));
 
 $req = $bdd->prepare('SELECT purchased_contents.id_users, users.credits FROM purchased_contents INNER JOIN users ON purchased_contents.id_users = users.id WHERE purchased_contents.id_contents = :id_contents');
 $req->execute(array(
@@ -68,7 +66,28 @@ if ($oldPrice > $newPrice) {
     }
 }
 
+if (isset($_FILES) && !empty($_FILES)) {
+    if (array_key_exists('content', $_FILES)) {
+        if ($_FILES['content']['error'] == 0) {
+            if (in_array($_FILES['content']['type'], ['video/mp4', 'image/png', 'image/jpeg'])) {
+                if ($_FILES['content']['size'] <= 128000000) {
+                    $content = uniqid() . '.' . pathinfo($_FILES['content']['name'], PATHINFO_EXTENSION);
+                    move_uploaded_file($_FILES['content']['tmp_name'], '../contents_img/' . $content);
+                } else {
+                    var_dump('Le fichier est trop volumineux…');
+                    exit;
+                }
+            } else {
+                echo 'Le type mime du fichier est incorrect…';
+            }
+        } else {
+            echo 'Le fichier n\'a pas pu être récupéré…';
+        }
+    }
+}
+
 if (!isset($content) && empty($content)) {
+
     $req = $bdd->prepare('UPDATE contents SET title = :title ,composer= :composer, level = :level, category = :category, price= :price, description = :description  WHERE id = :id');
     $req->execute(array(
         ':title' => $_POST['title'],
@@ -79,26 +98,6 @@ if (!isset($content) && empty($content)) {
         ':description' => $_POST['description'],
         ':id' => $_POST['id']
     ));
-
-    if ($_POST['category'] == 'Tutorial') {
-        $author_credits += 30;
-    } else if ($_POST['category'] == 'Performance') {
-        $author_credits += 10;
-    } else if ($_POST['category'] == 'Sheet Music') {
-        $author_credits += 20;
-    }
-
-    $req = $bdd->prepare('UPDATE users SET credits = :credits WHERE users.id = :users_id');
-    $req->execute(array(
-        ':credits' => $author_credits,
-        ':users_id' => $_POST['id_users']
-    ));
-
-    if ($_GET['type'] == 'admin') {
-        header('location: ../../admin/contents.php');
-    } else {
-        header('location: ../../single_player_content.php?id=' . $_POST['id']);
-    }
 } else {
 
     $requete = $bdd->prepare('SELECT * FROM contents WHERE id = :id');
@@ -118,25 +117,10 @@ if (!isset($content) && empty($content)) {
         ':content' => $content,
         ':id' => $_POST['id']
     ));
+}
 
-
-    if ($_POST['category'] == 'Tutorial') {
-        $author_credits += 30;
-    } else if ($_POST['category'] == 'Performance') {
-        $author_credits += 10;
-    } else if ($_POST['category'] == 'Sheet Music') {
-        $author_credits += 20;
-    }
-
-    $req = $bdd->prepare('UPDATE users SET credits = :credits WHERE users.id = :users_id');
-    $req->execute(array(
-        ':credits' => $author_credits,
-        ':users_id' => $_POST['id_users']
-    ));
-
-    if ($_GET['type'] == 'admin') {
-        header('location: ../../admin/contents.php');
-    } else {
-        header('location: ../../single_player_content.php?id=' . $_POST['id']);
-    }
+if ($_GET['type'] == 'admin') {
+    header('location: ../../admin/contents.php');
+} else {
+    header('location: ../../single_player_content.php?id=' . $_POST['id']);
 }
