d6de405b2f9c840765725533629d2d659f5c8964
chris <gitchris2@gmail.com>
2022-10-03 23:32:26 +0200

    Rapport03102332

---
+++ Rapport/SQL0310.sql
@@ -0,0 +1,225 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 03 oct. 2022 à 14:08
+-- Version du serveur : 5.7.36
+-- Version de PHP : 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `diplome`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment` text NOT NULL,
+  `date` text NOT NULL,
+  `likes` int(11) NOT NULL DEFAULT '0',
+  `id_contents` int(11) NOT NULL,
+  `id_users` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comments_id_contents_FK` (`id_contents`),
+  KEY `comments_id_users_FK` (`id_users`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` text NOT NULL,
+  `date` text NOT NULL,
+  `id_users` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `contact_id_users_FK` (`id_users`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contents`
+--
+
+DROP TABLE IF EXISTS `contents`;
+CREATE TABLE IF NOT EXISTS `contents` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `composer` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `level` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `likes` int(11) NOT NULL DEFAULT '0',
+  `reporting` int(11) NOT NULL DEFAULT '0',
+  `id_users` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `contents_id_users_FK` (`id_users`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `likes`
+--
+
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE IF NOT EXISTS `likes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_comments` int(11) DEFAULT NULL,
+  `id_contents` int(11) DEFAULT NULL,
+  `id_users` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `likes_id_users_FK` (`id_users`),
+  KEY `likes_id_comments_FK` (`id_comments`),
+  KEY `likes_id_contents_FK` (`id_contents`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `notifications`
+--
+
+DROP TABLE IF EXISTS `notifications`;
+CREATE TABLE IF NOT EXISTS `notifications` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `notification` text NOT NULL,
+  `date` text NOT NULL,
+  `id_users` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `notifications_id_users_FK` (`id_users`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `purchased_contents`
+--
+
+DROP TABLE IF EXISTS `purchased_contents`;
+CREATE TABLE IF NOT EXISTS `purchased_contents` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_contents` int(11) NOT NULL,
+  `id_users` int(11) NOT NULL,
+  `original_price` int(11) NOT NULL,
+  `buyer_repayment` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `purchased_contents_id_contents_FK` (`id_contents`),
+  KEY `purchased_contents_id_users_FK` (`id_users`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reporting`
+--
+
+DROP TABLE IF EXISTS `reporting`;
+CREATE TABLE IF NOT EXISTS `reporting` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` text NOT NULL,
+  `date` text NOT NULL,
+  `id_users` int(11) NOT NULL,
+  `id_contents` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `reporting_id_users_FK` (`id_users`),
+  KEY `reporting_id_contents_FK` (`id_contents`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL DEFAULT 'user',
+  `credits` int(11) NOT NULL DEFAULT '500',
+  `time` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_id_contents_FK` FOREIGN KEY (`id_contents`) REFERENCES `contents` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comments_id_users_FK` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `contact`
+--
+ALTER TABLE `contact`
+  ADD CONSTRAINT `contact_id_users_FK` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `contents`
+--
+ALTER TABLE `contents`
+  ADD CONSTRAINT `contents_id_users_FK` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_id_comments_FK` FOREIGN KEY (`id_comments`) REFERENCES `comments` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `likes_id_contents_FK` FOREIGN KEY (`id_contents`) REFERENCES `contents` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `likes_id_users_FK` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_id_users_FK` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `purchased_contents`
+--
+ALTER TABLE `purchased_contents`
+  ADD CONSTRAINT `purchased_contents_id_contents_FK` FOREIGN KEY (`id_contents`) REFERENCES `contents` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `purchased_contents_id_users_FK` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `reporting`
+--
+ALTER TABLE `reporting`
+  ADD CONSTRAINT `reporting_id_contents_FK` FOREIGN KEY (`id_contents`) REFERENCES `contents` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `reporting_id_users_FK` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
